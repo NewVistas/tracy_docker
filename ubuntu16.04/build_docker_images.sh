07bc50b5d4c24b984bbe06f12923f2d1553dcbf7
Kevin Li <kevin.li@norimaconsulting.com>
2018-11-21 12:44:24 -0600

    refine script and docker files.

--- ubuntu16.04/build_docker_images.sh
+++ ubuntu16.04/build_docker_images.sh
@@ -1,44 +1,145 @@
 #! /bin/bash
 
+DEV_MODE=false
+while [[ $# -gt 0 ]] && [[ ."$1" = .--* ]] ;
+do
+    opt="$1";
+    shift;              #expose next argument
+    case "$opt" in
+        "--devmode" )
+           DEV_MODE=true;;
+        *) echo >&2 "Invalid option: $@"; exit 1;;
+   esac
+done
+
 # build ubuntu base image
 docker image build -t ubuntu4tracy:16.04 -f ubuntu16.dockerfile .
+if [ $? -ne 0 ]; then
+  echo "ERROR on building ubuntu4tracy"
+  exit 1
+fi
 
 # build wannier
-docker image build -t wannier4tracy -f wannier.dockerfile .
+if [ ${DEV_MODE} == true ] ; then
+  docker image build -t wannier4tracy -f wannier.dockerfile . --build-arg DEV_MODE="YES"
+else
+  docker image build -t wannier4tracy -f wannier.dockerfile . --build-arg DEV_MODE="NO"
+fi
+if [ $? -ne 0 ]; then
+  echo "ERROR on building wannier4tracy"
+  exit 1
+fi
 
 # build dft_qe
-docker image build -t dft_qe4tracy:squash -f dft_qe.dockerfile .
+docker image build -t dft_qe4tracy:squash -f dft_qe.dockerfile . 
+if [ $? -ne 0 ]; then
+  echo "ERROR on building dft_qe4tracy:squash"
+  exit 1
+fi
+
 docker run -d --name=dft_qe4tracy_squash dft_qe4tracy:squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on running dft_qe4tracy_squash"
+  exit 1
+fi
+
 docker export dft_qe4tracy_squash | docker import - dft_qe4tracy
+if [ $? -ne 0 ]; then
+  echo "ERROR on exporting/importing dft_qe4tracy_squash"
+  exit 1
+fi
+
 docker stop dft_qe4tracy_squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on stopping dft_qe4tracy_squash"
+  exit 1
+fi
+
 docker rm dft_qe4tracy_squash
-#docker rmi dft_qe4tracy:squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on removing container dft_qe4tracy_squash"
+  exit 1
+fi
+
+docker rmi dft_qe4tracy:squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on removing image dft_qe4tracy:squash"
+  exit 1
+fi
+
 
 # build soapxx
-docker image build -t soap4tracy -f soap.dockerfile .
+docker image build -t soap4tracy -f soap.dockerfile . 
+if [ $? -ne 0 ]; then
+  echo "ERROR on building soap4tracy"
+  exit 1
+fi
 
 # build quip (serial version)
 docker image build -t quip4tracy -f quip.dockerfile .
+if [ $? -ne 0 ]; then
+  echo "ERROR on building quip4tracy"
+  exit 1
+fi
 
 # build lammps
-docker image build -t lammps4tracy -f lammps.dockerfile .
+docker image build -t lammps4tracy -f lammps.dockerfile . 
+if [ $? -ne 0 ]; then
+  echo "ERROR on building lammps4tracy"
+  exit 1
+fi
 
 #build pymatnest
-docker image build -t pymatnest4tracy -f pymatnest.dockerfile .
-
-#build mlip
-docker image build -t mlip4tracy -f mlip.dockerfile .
+docker image build -t pymatnest4tracy -f pymatnest.dockerfile . 
+if [ $? -ne 0 ]; then
+  echo "ERROR on building pymatnest4tracy" 
+  exit 1
+fi
 
 # build super image
-docker image build --squash -t tracy_science:squash -f tracy_science.dockerfile .
+docker image build --squash -t tracy_science:squash -f tracy_science.dockerfile . 
+if [ $? -ne 0 ]; then
+  echo "ERROR on building tracy_science:squash"
+  exit 1
+fi
+
 docker run -d --name=tracy_science_squash tracy_science:squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on running tracy_science_squash"
+  exit 1
+fi
+
 docker export tracy_science_squash | docker import - tracy_science
+if [ $? -ne 0 ]; then
+  echo "ERROR on exporting/importing tracy_science"
+  exit 1
+fi
+
 docker stop tracy_science_squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on stopping tracy_science_squash"
+  exit 1
+fi
+
 docker rm tracy_science_squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on removing container tracy_science_squash"
+  exit 1
+fi
+
 docker rmi tracy_science:squash
+if [ $? -ne 0 ]; then
+  echo "ERROR on removing image tracy_science:squash"
+  exit 1
+fi
+
 
 # build pslibrary image, this will be used by matdb. It's base on dft_qe4tracy
 docker image build -t pslibrary4tracy -f pslibrary.dockerfile .
+if [ $? -ne 0 ]; then
+  echo "ERROR on building image pslibrary4tracy"
+  exit 1
+fi
 
 # Use this command to start the docker container
 #docker run -it --rm --user=tracy tracy_science /bin/bash
--- ubuntu16.04/ubuntu16.dockerfile
+++ ubuntu16.04/ubuntu16.dockerfile
@@ -5,20 +5,19 @@ FROM ubuntu:16.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 
-RUN apt update; apt -yq upgrade; \
-    apt install -yq \
+RUN apt update && apt install -yq \
       autoconf  \
       ca-certificates  \
-      make  \
       cmake \
       g++ \
       gawk \
+      gdb \
       gfortran-5  \
       git \
       gnuplot \
+      libboost-all-dev \
       libgsl-dev \
       libreadline-dev \
-      libnetcdf-dev \
       libblacs-mpi-dev  \
       libblacs-openmpi1  \
       libgfortran-5-dev  \
@@ -26,50 +25,50 @@ RUN apt update; apt -yq upgrade; \
       libmatheval-dev \
       libopenblas-base  \
       libopenblas-dev  \
-      liblapack-dev \
       libopenmpi-dev  \
-      libxext-dev \
+      libnetcdf-dev \
+      liblapack-dev \
       libfftw3-3  \
       libfftw3-bin  \
       libfftw3-dev  \
       libfftw3-doc  \
       libfftw3-double3   \
+      libxext-dev \
+      make  \
+      nano \
       net-tools  \
       openssh-server  \
-      python \
-      python-pip \
-      python-dev \
-      python-setuptools \
       python3-numpy \
       python3-numpydoc \
       python3-scipy \
-      sudo \
-      nano \
-      vim \
-      wget  \
-      xxdiff \
-      zlib1g zlib1g-dev \
-      #{{ addtional packages needed for SOAP
-      software-properties-common \
-      libboost-all-dev \
+      python \
+      python-dev \
+      python-h5py \
+      python-pip \
+      python-setuptools \
+      python-sh \
       python-sklearn \
       python-psutil \
-      python-h5py \
-      python-sh \
-      #}}
+      python-tk \
+      software-properties-common \
+      sudo \
+      vim \
+      wget  \
       xdg-utils \
+      xxdiff \
+      zlib1g zlib1g-dev \
       --no-install-recommends \
     && apt autoremove   -y \
     && rm -rf /var/apt/lists/* \
     && ssh-keygen -A
 
 RUN python -m pip install --upgrade pip \
-    && python -m pip install mpi4py \
     && python -m pip install ase \
-    && python -m pip install --upgrade mpi4py \
-    && python -m pip install --upgrade ase \
     && python -m pip install boost \
-    && python -m pip install lxml
+    && python -m pip install lxml \
+    && python -m pip install mpi4py \
+    && python -m pip install --upgrade ase \
+    && python -m pip install --upgrade mpi4py 
 
 # we create the user 'tracy' and add it to the list of sudoers
 RUN adduser -q --disabled-password --gecos tracy tracy          \
